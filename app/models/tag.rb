5f854bca2c2c579df9d1b4244fd422b74bf8e3ea
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2023-01-14 10:21:00 +0000

    [Add]model記述

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,8 @@
 class Tag < ApplicationRecord
+
+  has_many :item_tags,dependent: :destroy, foreign_key: 'tag_id'
+  # タグは複数の投稿を持つ　それは、item_tagsを通じて参照できる
+  has_many :items,through: :item_tags
+
+  validates :name, uniqueness: true, presence: true
 end
